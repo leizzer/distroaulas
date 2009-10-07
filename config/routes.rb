c14f2ab6fe38cc03c633c33800dbed886b61be37
rayko <rayko@rayko-server.(none)>
2009-10-07 17:18:45 -0300

    New index page with basic links

--- config/routes.rb
+++ config/routes.rb
@@ -60,4 +60,5 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => 'application', :action => 'index'
 end
