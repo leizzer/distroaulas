4a6d9039309bdd6ea608401b753300ea2dd2f435
rayko <rayko@rayko-server.(none)>
2009-10-19 11:43:36 -0300

    (Ya no escribo mas en ingles) Se agrego un combo en las vistas edit y new del evento que lo unico que hace es mostrar una lista de materias y al elejir una, la pone en el text de la izquierda, solo para ayudar a la carga. En el index hay unas busquedas de pruebas que hay que mejorar y mover al controller. Se instalo el plugin calendar_date_select para usar un selector de fecha mas lindo, pero no se logro hacer andar. En el modelo de eventos, se agrego un metodo para replicar la fecha de inicio en la fecha de fin, para que el evento tenga inicio y fin en la misma fecha, variando solo la hora de inicio y fin.

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,5 +1,33 @@
 class Evento < ActiveRecord::Base
   validates_numericality_of :interval
+  before_save :end_date_set
 
   belongs_to :espacio
+
+ def end_date_set
+   temp = self.dtstart.year.to_s
+   if self.dtstart.month.to_s.length == 1
+     temp += "0" + self.dtstart.month.to_s
+   else
+     temp += self.dtstart.month.to_s
+   end
+   if self.dtstart.day.to_s.length == 1
+     temp += "0" + self.dtstart.day.to_s + "T"
+   else
+     temp += self.dtstart.day.to_s + "T"
+   end
+   if self.dtend.hour.to_s.length == 1
+     temp += "0" + self.dtend.hour.to_s 
+   else 
+     temp += self.dtend.hour.to_s 
+   end 
+   if self.dtend.min.to_s.length == 1
+     temp += "0" + self.dtend.min.to_s
+   else
+     temp += self.dtend.min.to_s
+   end
+   debugger
+   
+   self.dtend = DateTime.parse(temp)
+ end
 end
