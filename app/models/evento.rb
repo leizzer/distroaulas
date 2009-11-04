dc61b911b6933b7f5668ad3ee51ff9a422f75099
rayko <rayko@rayko-server.(none)>
2009-11-04 18:41:33 -0300

    Ahora se comprueba si se pisan, falta testearlo bien

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -2,7 +2,7 @@ class Evento < ActiveRecord::Base
   require 'ri_cal'
 
   validates_numericality_of :interval
-  before_save :set_dates, :check_dates
+  before_save :set_dates
 
   belongs_to :espacio
 
@@ -32,33 +32,40 @@ class Evento < ActiveRecord::Base
     self.dtend = DateTime.parse(temp)
    end
  
-  def check_dates
+  def validate
     calendar = RiCal.Calendar
-    events = Evento.find(:all, :conditions => "dtstart > '#{self.dtstart}' AND '#{self.dtstart + 1.day}' > dtstart AND reccurrent = 'f'") + Evento.find(:all, :conditions => { :reccurrent => true, :byday => self.dtstart.strftime("%a").upcase[0..1]})
+    events = Evento.find(:all, :conditions => "dtstart > '#{self.dtstart.to_date}' AND '#{self.dtstart.to_date + 1.day}' > dtstart AND reccurrent = 'f'") + Evento.find(:all, :conditions => { :reccurrent => true, :byday => self.dtstart.strftime("%a").upcase[0..1]})
 
     events.each do |event|
       temp = SimpEvent.new
       new_event = RiCal.Event
       new_event.description = event.description
-      new_event.dtstart = event.dtstart.strftime '%Y%m%dT%H%M00'
-      new_event.dtend = event.dtend.strftime '%Y%m%dT%H%M00'
+      new_event.dtstart = event.dtstart #.strftime '%Y%m%dT%H%M00'
+      new_event.dtend = event.dtend #.strftime '%Y%m%dT%H%M00'
       new_event.location = event.espacio_id.to_s
       new_event.rrule = "FREQ=" + event.freq + ";BYDAY=" + event.byday + ";INTERVAL=" + event.interval.to_s if event.reccurrent
       new_event.exdates = event.exdate.to_a
-      new_event.rdates = event.rdate.to_a
+      new_event.rdates = event.rdate.to_a,
+
       #Occurrences te va a manejar automaticamente las exdates y rdates, no tenes que hacer ningun otro calculo mas que cargarlos al evento.
       #Creo que son las primeras lineas de comentario en TODA la aplicacion XD Mal
       if event.reccurrent
-        occurrence = new_event.occurrences :count => 1, :starting => self.dtstart, :before => self.dtstart + 1
+        occurrence = new_event.occurrences :count => 1, :starting => self.dtstart.to_date, :before => self.dtstart.to_date + 1
         if occurrence.count > 0
           calendar.add_subcomponent new_event
         end
       else
-        calendar.add_subcomponent new_event if (Date.parse(event.dtstart.year.to_s + '/' +  event.dtstart.month.to_s + '/' + event.dtstart.day.to_s)) == self.dtstart
+        calendar.add_subcomponent new_event if (Date.parse(event.dtstart.year.to_s + '/' +  event.dtstart.month.to_s + '/' + event.dtstart.day.to_s)) == self.dtstart.to_date
       end
     end
-    debugger
+    calendar.events.each do |event|
 
-    asd = 1
+      if self.dtstart.between? event.dtstart, event.dtend
+        errors.add('Inicio: ', "Sobrecarga -> #{Espacio.find(:first, :conditions => {:id => event.location.to_i}).codigo} - #{event.description} - de #{event.dtstart.strftime('%H:%M')} a #{event.dtend.strftime('%H:%M')}")
+      end
+      if self.dtend.between? event.dtstart, event.dtend
+        errors.add('Finalizacion: ', "Sobrecarga -> #{Espacio.find(:first, :conditions => {:id => event.location.to_i}).codigo} - #{event.description} - de #{event.dtstart.strftime('%H:%M')} a #{event.dtend.strftime('%H:%M')}")
+      end
+    end
   end
 end
